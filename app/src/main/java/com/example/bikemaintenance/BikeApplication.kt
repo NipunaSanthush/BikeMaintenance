2a43b61729b56fea7cfeccc6940fe8105299b8d3
Nipuna Santhush <nipunasanthush2004@gmail.com>
2025-11-30 14:15:20 +0530

    Create BikeApplication.kt

---
+++ app/src/main/java/com/example/bikemaintenance/BikeApplication.kt
@@ -0,0 +1,12 @@
+package com.example.bikemaintenance
+
+import android.app.Application
+import com.example.bikemaintenance.data.BikeDatabase
+import com.example.bikemaintenance.data.MaintenanceRepository
+
+class BikeApplication : Application(){
+
+    val database by lazy { BikeDatabase.getDatabase(this) }
+
+    val repository by lazy { MaintenanceRepository(database.maintenanceDao()) }
+}
