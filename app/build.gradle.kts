bbb6da1d9cbaa54016a529577411a8a4c7d0d588
Nipuna Santhush <nipunasanthush2004@gmail.com>
2025-12-04 19:52:42 +0530

    Add Library Plugin
    ViewModels

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,4 +57,5 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycle_version")
     implementation("androidx.lifecycle:lifecycle-common-java8:$lifecycle_version")
+    implementation("androidx.fragment:fragment-ktx:1.8.2")
 }
