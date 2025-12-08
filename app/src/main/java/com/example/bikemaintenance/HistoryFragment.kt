c203644c31050c9e13c889ead96b5593d1b175ee
Nipuna Santhush <nipunasanthush2004@gmail.com>
2025-12-08 21:18:12 +0530

    Create a History Logic

--- app/src/main/java/com/example/bikemaintenance/HistoryFragment.kt
+++ app/src/main/java/com/example/bikemaintenance/HistoryFragment.kt
@@ -1,13 +1,17 @@
 package com.example.bikemaintenance
 
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.bikemaintenance.adapter.FuelAdapter
 import com.example.bikemaintenance.adapter.MaintenanceAdapter
 import com.example.bikemaintenance.viewmodel.MaintenanceViewModel
 import com.example.bikemaintenance.viewmodel.MaintenanceViewModelFactory
@@ -29,15 +33,36 @@ class HistoryFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         val recyclerView = view.findViewById<RecyclerView>(R.id.rvHistory)
-        val adapter = MaintenanceAdapter()
+        val btnServices = view.findViewById<Button>(R.id.btnShowServices)
+        val btnFuel = view.findViewById<Button>(R.id.btnShowFuel)
 
-        recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
 
+        val serviceAdapter = MaintenanceAdapter()
+        val fuelAdapter = FuelAdapter()
+
+        recyclerView.adapter = serviceAdapter
+
         maintenanceViewModel.allRecords.observe(viewLifecycleOwner) { records ->
-            records?.let {
-                adapter.submitList(it)
-            }
+            serviceAdapter.submitList(records)
+        }
+
+        maintenanceViewModel.allFuelRecords.observe(viewLifecycleOwner) { records ->
+            fuelAdapter.submitList(records)
+        }
+
+        btnServices.setOnClickListener {
+            recyclerView.adapter = serviceAdapter
+
+            btnServices.backgroundTintList = ColorStateList.valueOf(Color.parseColor("#1565C0"))
+            btnFuel.backgroundTintList = ColorStateList.valueOf(Color.parseColor("#B0BEC5"))
+        }
+
+        btnFuel.setOnClickListener {
+            recyclerView.adapter = fuelAdapter
+
+            btnFuel.backgroundTintList = ColorStateList.valueOf(Color.parseColor("#1565C0"))
+            btnServices.backgroundTintList = ColorStateList.valueOf(Color.parseColor("#B0BEC5"))
         }
     }
 }
