cf6476fbab43feeb5e745add2816b2485430344e
Nipuna Santhush <nipunasanthush2004@gmail.com>
2025-12-10 22:20:52 +0530

    Connect Trip DAO to the Repository

--- app/src/main/java/com/example/bikemaintenance/data/MaintenanceRepository.kt
+++ app/src/main/java/com/example/bikemaintenance/data/MaintenanceRepository.kt
@@ -4,26 +4,27 @@ import kotlinx.coroutines.flow.Flow
 
 class MaintenanceRepository(
     private val maintenanceDao: MaintenanceDao,
-    private val fuelDao: FuelDao
+    private val fuelDao: FuelDao,
+    private val tripDao: TripDao
 ){
 
     val allRecords: Flow<List<MaintenanceRecord>> = maintenanceDao.getAllRecords()
-
     suspend fun insert(record: MaintenanceRecord){
         maintenanceDao.insertRecord(record)
     }
-
     suspend fun update(record: MaintenanceRecord){
         maintenanceDao.updateRecord(record)
     }
-
     suspend fun delete(record: MaintenanceRecord){
         maintenanceDao.deleteRecord(record)
     }
 
     val allFuelRecords: Flow<List<FuelRecord>> = fuelDao.getAllFuelRecords()
-
     suspend fun insertFuel(record: FuelRecord){
         fuelDao.insertFuelRecord(record)
     }
+
+    val allTrips: Flow<List<TripRecord>> = tripDao.getAllTrips()
+    suspend fun insertTrip(trip: TripRecord) =tripDao.insertTrip(trip)
+    
 }
