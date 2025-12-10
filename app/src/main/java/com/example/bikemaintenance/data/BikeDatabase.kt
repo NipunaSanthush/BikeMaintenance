88b30280dd7de597de932af3e2cb4b0f5f7c5c10
Nipuna Santhush <nipunasanthush2004@gmail.com>
2025-12-10 22:06:31 +0530

    Update Database about Trip DAO

--- app/src/main/java/com/example/bikemaintenance/data/BikeDatabase.kt
+++ app/src/main/java/com/example/bikemaintenance/data/BikeDatabase.kt
@@ -5,11 +5,12 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [MaintenanceRecord::class, FuelRecord::class], version = 2, exportSchema = false)
+@Database(entities = [MaintenanceRecord::class, FuelRecord::class, TripRecord::class], version = 3, exportSchema = false)
 abstract class BikeDatabase : RoomDatabase(){
 
     abstract fun maintenanceDao(): MaintenanceDao
     abstract fun fuelDao(): FuelDao
+    abstract fun tripDao(): TripDao
 
     companion object{
         @Volatile
