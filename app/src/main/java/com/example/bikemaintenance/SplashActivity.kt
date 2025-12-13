073e8ac9c279cae7bf799ba0c25445b4a7ab6a5a
Nipuna Santhush <nipunasanthush2004@gmail.com>
2025-12-14 01:28:22 +0530

    UI Changes

--- app/src/main/java/com/example/bikemaintenance/SplashActivity.kt
+++ app/src/main/java/com/example/bikemaintenance/SplashActivity.kt
@@ -1,15 +1,27 @@
 package com.example.bikemaintenance
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import com.example.bikemaintenance.utils.SessionManager
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val sharedPrefs = getSharedPreferences("AppSettings", Context.MODE_PRIVATE)
+        val isDarkMode = sharedPrefs.getBoolean("DARK_MODE", false)
+
+        if (isDarkMode) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
+
         setContentView(R.layout.activity_splash)
 
         Handler(Looper.getMainLooper()).postDelayed({
