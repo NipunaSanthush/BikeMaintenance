6e19bf99c981bb7f026ef0baec9fc2afca674600
Nipuna Santhush <nipunasanthush2004@gmail.com>
2025-12-16 22:41:44 +0530

    Add Google Location Library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,4 +80,6 @@ dependencies {
     implementation("com.github.bumptech.glide:glide:4.16.0")
 
     implementation("com.github.yalantis:ucrop:2.2.8")
+
+    implementation("com.google.android.gms:play-services-location:21.0.1")
 }
