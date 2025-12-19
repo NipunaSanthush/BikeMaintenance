2aa5790e3667d5fdae4228815c9cf220a32b2379
Nipuna Santhush <nipunasanthush2004@gmail.com>
2025-12-19 23:59:40 +0530

    Update SignUpActivity.kt

--- app/src/main/java/com/example/bikemaintenance/SignUpActivity.kt
+++ app/src/main/java/com/example/bikemaintenance/SignUpActivity.kt
@@ -49,7 +49,7 @@ class SignUpActivity : AppCompatActivity() {
         }
 
         val brands = listOf("Honda", "Yamaha", "Bajaj", "TVS", "Suzuki", "Other")
-        val brandAdapter = ArrayAdapter(this, R.layout.spinner_item, brands)
+        val brandAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, brands)
         brandAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spinnerBrand.adapter = brandAdapter
 
